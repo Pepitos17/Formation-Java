3966b64dda55c969e997bfdad4cf73bcf0ac631a
Pepitos17 <msogodogo@gmail.com>
2022-02-07 19:58:17 +0100

    premier commit

---
+++ hellopackage/HelloUniverse.java
@@ -0,0 +1,30 @@
+package hellopackage;
+
+public class HelloUniverse {
+    public static void main(String... args) {
+
+        /*exo1
+        System.out.println("Hello Universe !")*/
+        ;
+
+        /*exo 2
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
+        int nbPlanetes=8;
+        System.out.println(nbPlanetes);
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : ");
+        nbPlanetes=9;
+        System.out.println(nbPlanetes);*/
+
+        /*exo3
+        String message = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        System.out.println(message);
+        int nbPlanetes=8;
+        System.out.println(nbPlanetes);
+         message ="Il y a quelques années cependant, elles étaient au nombre de :";
+        System.out.println(message);
+        nbPlanetes=9;
+        System.out.println(nbPlanetes);*/
+
+
+    }
+}
